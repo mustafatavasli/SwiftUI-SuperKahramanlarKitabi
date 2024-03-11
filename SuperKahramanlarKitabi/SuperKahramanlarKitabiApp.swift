e2146e715a61eb77a0ed039d745cf5bd1f358f03
mustafatavasli <musstafatavasli@gmail.com>
2024-03-11 17:57:34 +0300

    Initial Commit

---
+++ SuperKahramanlarKitabi/SuperKahramanlarKitabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanlarKitabiApp.swift
+//  SuperKahramanlarKitabi
+//
+//  Created by Mustafa TAVASLI on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanlarKitabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
